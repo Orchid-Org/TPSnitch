3ad4af1cd58babe04e425652eef8c2c863d085c4
dashtiss <brandontisserand@outlook.com>
2026-03-10 20:45:09 -0500

    Initial commit for new version

---
+++ src/main/java/io/github/orchidorg/tpsnitch/Tpsnitch.java
@@ -0,0 +1,11 @@
+package io.github.orchidorg.tpsnitch;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tpsnitch implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
